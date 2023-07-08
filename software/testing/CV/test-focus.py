c6031cca08f73533cbdf3560e8d58d203b11d334
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-07-07 20:02:46 -0700

    Update test-focus.py

--- software/testing/CV/test-focus.py
+++ software/testing/CV/test-focus.py
@@ -7,7 +7,7 @@ def variance_of_laplacian(image):
 	# measure, which is simply the variance of the Laplacian
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
-array = [0, 5, 15, 20, 25, 30] # test different focus
+array = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19] # test different focus
 
 # Test if focus can be changed using the v4l1 utils lib
 i = 0 
