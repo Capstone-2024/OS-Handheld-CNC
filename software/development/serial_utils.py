bd6743118b011bdf33a415c3d7a24672c9d6d8a7
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-11-29 18:16:36 -0500

    Update serial_utils.py

--- software/development/serial_utils.py
+++ software/development/serial_utils.py
@@ -75,7 +75,7 @@ class ArduinoComms:
         return self
 
     def ardu_read(self):
-        self.data = self.arduino.read().decode()
+        self.data = self.arduino.readline().decode()
 
     def close():
         arduino.close()
