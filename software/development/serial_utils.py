6f0ff1a38acc71457b6285b0fe65669bcceb8684
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2023-11-29 19:55:17 -0500

    Update serial_utils.py

--- software/development/serial_utils.py
+++ software/development/serial_utils.py
@@ -85,45 +85,37 @@ class ArduinoComms:
 
 
 if __name__ == "__main__":
-    # port = ''
+    port = ''
 
-    # for device in serial.tools.list_ports.comports(): 
-    #     print(device.description)
-    #     if 'USB Serial' in device.description: 
-    #         port = device.device
+    for device in serial.tools.list_ports.comports(): 
+        print(device.description)
+        if 'USB Serial' in device.description: 
+            port = device.device
             
-    # try:
-    #     arduino = serial.Serial(
-    #         port, 115200, timeout=1
-    #     )
-    #     arduino.flushInput()
-    #     arduino.flushOutput()
+    try:
+        arduino = serial.Serial(
+            port, baudrate=115200
+        )
+        arduino.flush()
 
-    #     # arduino.write('H'.encode('ascii'))
-    #     arduino.write('A'.encode())
-    #     time.sleep(1)
-    #     # data = arduino.readline()
+        # arduino.write('H'.encode('ascii'))
+        arduino.write('A'.encode())
+        time.sleep(1)
+        # data = arduino.readline()
     
-    #     data = arduino.readline()
-    #     print(data)
+        data = arduino.readlines()
+        print(data)
 
-    #     arduino.close()
-            
-    #     #     print(data)
-    #     # else: 
-    #     #     print("No Data")
+        arduino.close()
 
-    # except serial.SerialException as e:
-    #     print(f"Error: {e}")
+    except serial.SerialException as e:
+        print(f"Error: {e}")
 
 
-    arduino = ArduinoComms()
-    # arduino.start_transmit()
-    arduino.ardu_write('A'.encode('ascii'))
-    # arduino.start_read()
-    arduino.ardu_read(10)
-    print(arduino.arduino.readline())
-    print(arduino.data)
-
-    # arduino.ardu_read(4)
+    # arduino = ArduinoComms()
+    # # arduino.start_transmit()
+    # arduino.ardu_write('A'.encode('ascii'))
+    # # arduino.start_read()
+    # arduino.ardu_read(10)
+    # print(arduino.arduino.readline())
     # print(arduino.data)
