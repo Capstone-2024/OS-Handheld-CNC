9242af5938cd2a3d3f6fe2ce5a2879cf2e5ee938
“simu33gao” <151412787+simu33gao@users.noreply.github.com>
2023-12-04 22:22:28 -0500

    Update main_final.py

--- software/development/main_final.py
+++ software/development/main_final.py
@@ -599,7 +599,7 @@ class StichingWin(QWidget, StichingUi):
     def capture(self):
         # 此处0表示默认摄像头，1不是默认，准确判断需要尝试,比如1,2,3之类
         # 使用OpenCV库访问摄像头
-        self.camera = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(1)
         # 检查摄像头是否成功打开
         if not self.camera.isOpened():
             # 如果摄像头未成功打开，弹出错误提示框
